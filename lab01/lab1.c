44ead021ccb9d15de868f0907b6f100ad36bf3db
ghos7ie <elyreum7@gmail.com>
2023-02-01 13:15:41 +0800

    lab 1 complete

---
+++ lab01/lab1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void){
+	int a = 100;
+	int b = 10;
+	int c = a+b;
+	int d = a-b;
+	// Change a/d to b/a, since result is fractional, will be truncated
+	int e = b/a; 
+	int f = a*b;
+	int i;
+	char *day[7] = {
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+		"Sunday"
+	};
+
+	printf("Arithmetic operations:\n");
+	printf("a+b = %d\n", c);
+	printf("a-b = %d\n", d);
+	printf("b/a = %d\n", e);
+	printf("a*b = %d\n\n", f);
+
+	printf("Days of the week:\n");
+	for (i=0; i<8; i++) {
+		printf("Day[%d] = %s\n", i, day[i]);
+	}
+
+	return 0;
+}
